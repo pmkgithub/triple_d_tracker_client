3b7f7b36417a6f371b5ac6aef637c01504630a0d
Peter King <73837king@gmail.com>
2018-07-27 07:54:26 -0400

    added React Font Awesome, added green check to visted restaurants in Filtered Locations List

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import { library as faLibrary} from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+faLibrary.add(faCheck);
 
 export default ({children}) => {
   return (
