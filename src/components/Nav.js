7a594daa8bbf9d7f69d68597e34e7342c6d6243f
Peter King <73837king@gmail.com>
2018-08-15 14:59:06 -0400

    Fixed routing logic error.  Fixes cases when User closes the Browser with signing out.  When User re-opens a Browser, he will be redirected to /dashboard if the Browser address is /, /signup or /signin.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { signout } from '../actions';
 import './nav.css';
 
-class Header extends Component {
+class Nav extends Component {
 
   renderLinks() {
+    // console.log('Nav.js this.props.authenticated = ', this.props.authenticated);
+    console.log('Nav.js this.props.currentRoute = ', this.props.currentRoute);
 
     if (!this.props.authenticated && this.props.currentRoute === '/') {
       return (
@@ -44,6 +47,7 @@ class Header extends Component {
     }
 
     if (this.props.authenticated && this.props.currentRoute === '/dashboard') {
+      console.log('if (this.props.authenticated && this.props.currentRoute === \'/dashboard\') ran');
       return (
         <div className="nav_links_wrapper">
           <Link to="/about">About</Link>
@@ -53,12 +57,31 @@ class Header extends Component {
     }
 
     if (this.props.authenticated && this.props.currentRoute === '/about') {
+      console.log('if (this.props.authenticated && this.props.currentRoute === \'/about\') ran');
       return (
         <div className="nav_links_wrapper">
           <Link to="/dashboard">Dashboard</Link>
           <Link to="/signout">Sign Out</Link>
         </div>
       )
+    }
+
+    // Handle cases when User closes Browser without signing out,
+    // He will still be logged in when the Browser is opened at a later time.
+    // Redirect the User back to the Dashboard.
+    if (this.props.authenticated && this.props.currentRoute === '/') {
+      window.location = '/dashboard';
+      return false;
+    }
+
+    if (this.props.authenticated && this.props.currentRoute === '/signup') {
+      window.location = '/dashboard';
+      return false;
+    }
+
+    if (this.props.authenticated && this.props.currentRoute === '/signin') {
+      window.location = '/dashboard';
+      return false;
     }
 
   }
@@ -79,4 +102,4 @@ const mapStateToProps = (state) => {
   }
 };
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, { signout })(Nav);
